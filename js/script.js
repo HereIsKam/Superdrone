f5db6ed7592dd7060440c3fd656c57965795a3e2
HereIsKam <HereIsKam@users.noreply.github.com>
2020-05-17 18:10:49 +0300

    Added new screens
    Video slider, Parameters

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+  prevArrow: '.btn-left',
+  nextArrow: '.btn-right'
+});
+
+$('.appearance-slider').slick({
+  centerMode: true,
+  slidesToShow: 1,
+  centerPadding: '100px',
+  prevArrow: '.btn-left',
+  nextArrow: '.btn-right'
+})
